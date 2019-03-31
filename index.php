e85ea022028aa692ef524671e2f7f698ed3701ed
tomthetank46 <tomhogrefe46@gmail.com>
2019-03-31 19:39:59 -0400

    updated index

--- index.php
+++ index.php
@@ -3,19 +3,40 @@
 	<img src="images/blue-c.jpeg" id="home" alt="Lone cup of espresso drink used as pretty image fill-in">
 	<div class="row" id="nail-down">
 		<div class="column buffer">
-			<blockquote>
+			<!-- <blockquote>
 			    The fresh smell of [espresso]
 			    <br> soon wafted through the apartment,
 			    <br>the smell that separates night from day.
-			</blockquote>
+			</blockquote> -->
 			<!-- I know an angel cries everytime <br> is used, but it's OK to use it for poems! -->
 
-			<cite>– Haruki Murakami, Colorless Tsukuru Tazaki</cite>
+			<!-- <cite>– Haruki Murakami, Colorless Tsukuru Tazaki</cite> -->
+
+			<h2>Previous Articles</h2>
+
+			<div class="preview row">
+			<!-- <a href="details.php"> -->
+			<h3>Espresso</h3>
+				<p>Before we can describe the complex drinks that use espresso as their bases, we must understand what the espresso is!  While espressos are great mixed with milk, they are often enjoyed alone.  These are not for the faint of heart, though.</p>
+				<img src="images/espresso.jpg" alt="A traditional espresso">
+			<!-- </a> -->
+			</div>
+
+			<div class="preview row">
+			<!-- <a href="details.php"> -->
+			<h3>Macchiato</h3>
+				<p class="preview">No, this isn't the same macchiato they serve in Starbucks!  The traditional macchiato is actually a lot smaller and different from what you're imagining in your head.  Macchiatos are essentially a less bitter counterpart to the espresso shot (given the extra bit of foam they have).</p>
+				<img src="images/macchiato.jpg" alt="A traditional macchiato">
+			<!-- </a> -->
+			</div>
 		</div>
 
 		<div class="buffer" id="only">
 			<a href="details.php">
 				<div class="button-imp">Don't be a fool at the coffee shop. Learn about espresso now!</div>
+			</a>
+			<a href="newsletter.php">
+				<div class="button-imp">Never pay full price again. Sign up for the newsletter!</div>
 			</a>
 		</div>
 	</div>
