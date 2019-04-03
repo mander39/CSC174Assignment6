62d8ba675bdf59c11d3cfbd93d97e4d105bac550
mander39 <m.anderson996@gmail.com>
2019-04-03 15:23:51 -0400

    error fixing

--- thanks.php
+++ thanks.php
@@ -4,7 +4,7 @@
 		<h2>Thank you</h2>
 		<p>Thanks for signing up! Look forward to some freshly-roasted coffee information in your inbox.</p>
 	</header>
-	<div><a href="index.php">Return to homepage.</div></p>
+	<div><a href="index.php">Return to homepage.</a></div>
 </main>
 </body>
 </html>
